60343912e2c6e514cc55483db6f6f290c8dc33ae
Mohamed Essam Salem <70966283+MohamedESalem@users.noreply.github.com>
2022-04-04 08:09:15 +0200

    Update JavaMail.java

--- src/email/JavaMail.java
+++ src/email/JavaMail.java
@@ -4,6 +4,6 @@ package email;
 
 public class JavaMail {
     public static void main(String[] args) throws Exception {
-        JavaMailUtil.sendMail("mohamed.e.salem@protonmail.com");
+        JavaMailUtil.sendMail("Emailaddress@mail.com");
     }
 }
